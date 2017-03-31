85a2fe0b639ffa5d5480e4b4f37c24b6e08bf01f
WookCheolShin <shinwc159@gmail.com>
2017-03-31 22:07:09 +0900

    코드 정리 및 모듈화,

--- imu.c
+++ imu.c
@@ -1,65 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-
-#include <linux/i2c.h>
-#include <linux/i2c-dev.h>
 #include "myahrs_plus.h"
+/*
+* imu 예제 프로그램.
+* 각각의 데이터를 받아와 프린트.
+*/
 
-#define I2C_DEV_FILENAME    "/dev/i2c-1"
-#define AHRS_SLAVE_ADDRESS 0x20
-
-void print_imu(ahrs_data_t* ahrs_data);
+void print_imu(ahrs_sensor_t *sensor, ahrs_euler_t *euler, ahrs_quaternian_t *quat)
+{
+    printf("\r\nIMU DATA\r\n");
+    printf("ACC[g]:%.2f,%.2f,%.2f\r\n", sensor->acc_x, sensor->acc_y, sensor->acc_z);
+    printf("GYRO[deg/s]:%.2f,%.2f,%.2f\r\n", sensor->gyro_x, sensor->gyro_y, sensor->gyro_z);
+    printf("MAG[uT]:%.2f,%.2f,%.2f\r\n", sensor->magnet_x, sensor->magnet_y, sensor->magnet_z);
+    printf("R/P/Y[deg]:%.2f,%.2f,%.2f\r\n", euler->roll,euler->pitch, euler->yaw);
+    printf("Quaternian:%.4f,%.4f,%.4f,%.4f\r\n", quat->x, quat->y, quat->z, quat->w);
+}
 
 void main(int argc, char * argv[])
 {
-    int fd=0,rtn=0,i=0,j =0;
-    unsigned char buf[30] = {0,};
+    int j =0;
     unsigned char ret = -1;
+    unsigned char buf[3] ={0,};
 
-    ahrs_data_t * ahrs_data = malloc(sizeof(ahrs_data_t));
+    ahrs_sensor_t     * p_sensor = malloc(sizeof(ahrs_sensor_t));
+    ahrs_euler_t      * p_euler  = malloc(sizeof(ahrs_euler_t));
+    ahrs_quaternian_t * p_quat   = malloc(sizeof(ahrs_quaternian_t));
     
     //i2c 디바이스파일을 오픈
-    if((fd = open( I2C_DEV_FILENAME, O_RDWR )) < 0){
-        perror("open()");
+    if((ret = i2c_myahrs_setup())<0){
+        perror("imu setup");
         exit(1);
     }else
-        printf("I2C Device Driver Open Sucess!\n");
+        printf("myAHSR+ Device Setup Sucess!\n");
+        
+    if((ret = i2c_myahrs_state_check()) < 0){
+        perror("imu state");
+        exit(1);
+    }else 
+        printf("myAHSR+ Device Connecting OK!\n");
 
-    //접근할 디바이스의 슬레이브 주소를 설정합니다.
-    if((rtn = ioctl(fd, I2C_SLAVE, AHRS_SLAVE_ADDRESS)) < 0){
-        perror("ioctl");
-        close(fd);
-        exit(1);
-    }else
-        printf("myAHSR+ Device Select Sucess!\n");
+    while(j++ != 10){
         
-    do{
+        if((ret = i2c_myahrs_raw_sensor_read(p_sensor))<0)
+            break;
+        usleep(100000);
         
-        ret = I2C_MYAHRS_READ(fd,CAL_DATA,ahrs_data);
-        print_imu(ahrs_data);
-        
-        sleep(1);
-        
+        if((ret = i2c_myahrs_cal_sensor_read(p_sensor))<0)
+            break;
+        usleep(100000);
+
+        if((ret = i2c_myahrs_euler_read(p_euler))<0)
+            break;
+        usleep(100000);
+
+        if((ret = i2c_myahrs_quat_read(p_quat))<0)
+            break;
+        usleep(100000);
+
         #if 0
         //Read test 
-        ret = Read(fd,buf,2,REG_TEMPERATURE_LOW);
+        ret = i2c_read(buf,2,REG_TEMPERATURE_LOW);
         printf("buf : 0x%x 0x%x \t",buf[0],buf[1]);
         printf("temp : %.2f \n",(float)(signed short)(buf[1]<<8|buf[0])*200/32767);
         #endif
+        print_imu(p_sensor,p_euler,p_quat);
     }
-    while(j++ != 50);
-
-    free(ahrs_data);
-    close(fd);
-}
-
-void print_imu(ahrs_data_t* ahrs_data)
-{
-    printf("ACC[g]:%.2f,%.2f,%.2f\r\n", ahrs_data->sensor.acc_x, ahrs_data->sensor.acc_y, ahrs_data->sensor.acc_z);
-    printf("GYRO[deg/s]:%.2f,%.2f,%.2f\r\n", ahrs_data->sensor.gyro_x, ahrs_data->sensor.gyro_y, ahrs_data->sensor.gyro_z);
-    printf("MAG[uT]:%.2f,%.2f,%.2f\r\n", ahrs_data->sensor.magnet_x, ahrs_data->sensor.magnet_y, ahrs_data->sensor.magnet_z);
-    printf("Temperature[degC]:%.2f\r\n", ahrs_data->temp);
-    printf("R/P/Y[deg]:%.2f,%.2f,%.2f\r\n", ahrs_data->euler.roll,ahrs_data->euler.pitch, ahrs_data->euler.yaw);
-    printf("Quaternian:%.4f,%.4f,%.4f,%.4f\r\n", ahrs_data->q.x, ahrs_data->q.y, ahrs_data->q.z, ahrs_data->q.w);
+    
+    free(p_sensor);
+    free(p_euler);
+    free(p_quat);
 }
